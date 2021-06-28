72e76317122863307be1090e88341fb194c4b940
Mu-munn <rpgmukai333@gmail.com>
2021-06-28 09:35:54 +0900

    update

--- main.js
+++ main.js
@@ -67,3 +67,25 @@ window.onload = function() {
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
+/** humberger */
+var navFlg = false;
+$('.menu').on('click',function(){
+    $('.menu__line').toggleClass('active');
+    $('.gnav').fadeToggle();
+    if(!navFlg){
+        $('.gnav__menu__item').each(function(i){
+            $(this).delay(i*300).animate({
+                'margin-left' : 0,
+                'opacity' : 1,
+            },500);
+        });
+        navFlg = true;
+    }
+    else{
+        $('.gnav__menu__item').css({
+            'margin-left' : 100,
+            'opacity' : 0,
+        });
+        navFlg = false;
+    }
+});
